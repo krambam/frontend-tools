9e0484fee97f6b2b5d1238bdc30c055dcfc82ab9
Nicolas Wanner <cutslade@hotmail.com>
2016-08-30 06:10:39 +0200

    fix

--- dist/medialisk-frontend-tools.js
+++ dist/medialisk-frontend-tools.js
@@ -10,22 +10,40 @@ var mmenu = require('jquery.mmenu');
                 pageWrapId: 'mm-page-wrap',
                 navId: 'main-nav',
                 mobnavTriggerId: 'mobnav-trigger',
-                elementsToWrapSelector: 'body > header, body > section, body > div, body > footer'
+                elementsToWrapSelector: 'body > header, body > section, body > div, body > footer, body > nav, body > article',
+                mmenuOptions: {}
             };
             var opts = $.extend({}, defaultOptions, optionsObject);
 
-            $( opts.elementsToWrapSelector ).wrapAll( '<div id="' + opts.pageWrapId + '" />');
-            $('#' + opts.navId).mmenu({},{
-                classNames: {
+            // mmenuConfig extend and default
+            var defaultMmenuConfig = {
+                className: {
                     selected: "active"
                 },
                 offCanvas: {
                     pageSelector: '#' + opts.pageWrapId
                 },
                 clone: true
-            });
+            };
+            if(optionsObject.mmenuConfig){
+                var extended = $.extend({}, defaultMmenuConfig, optionsObject.mmenuConfig);
+                opts.mmenuConfig = extended;
+            }else{
+                opts.mmenuConfig = defaultMmenuConfig;
+            }
+
+            $( opts.elementsToWrapSelector ).wrapAll( '<div id="' + opts.pageWrapId + '" />');
+
+            $('#' + opts.navId).mmenu( opts.mmenuOptions, opts.mmenuConfig);
+
+            var mobileNavSelector = opts.navId;
+            if(opts.mmenuConfig.clone) {
+                mobileNavSelector = '#mm-' + mobileNavSelector; // cloning makes a prefix
+            } else {
+                mobileNavSelector = '#' + mobileNavSelector;
+            }
             $('#' + opts.mobnavTriggerId ).click(function() {
-                $('#mm-' + opts.navId ).data( "mmenu" ).open();
+                $(mobileNavSelector).data( "mmenu" ).open();
             });
         }
     
