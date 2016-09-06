47d5412860a54d9fb94f92b2d0c2c188eddd2ef0
Nicolas Wanner <cutslade@hotmail.com>
2016-09-06 10:14:20 +0200

    x

--- build.sh
+++ build.sh
@@ -1,1 +1,5 @@
-echo hello
+#!/bin/bash
+ 
+NPMVER="$(npm ls | grep  -o ^[A-Za-z].*@[[:digit:]][[:digit:]]*[[:digit:]]*\.[[:digit:]][[:digit:]]*[[:digit:]]*\.[[:digit:]][[:digit:]]*[[:digit:]]* | grep -o @.*\..*\..*$ | grep -o [[:digit:]].*)"
+GITVER="$(git describe | grep -o [[:digit:]][[:digit:]]*[[:digit:]]*\.[[:digit:]][[:digit:]]*[[:digit:]]*\.[[:digit:]][[:digit:]]*[[:digit:]]*\.- | grep -o [[:digit:]].*\.[[:digit:]].*\.[[:digit:]]*- | sed "s/.$//g")"
+echo "${GITVER}"="${NPMVER}"
